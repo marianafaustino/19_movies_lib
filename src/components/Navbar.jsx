08a33a6b42b094ae0b0c6f382e45038ad78f1f82
Mariana Faustino <marifaustino@gmail.com>
2023-07-12 16:11:35 -0300

    CSS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom"
 import {BiCameraMovie, BiSearchAlt2} from 'react-icons/bi'
+import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
+    <nav id="navbar">
         <h2>
           <Link to="/"><BiCameraMovie/>MoviesLib</Link>
         </h2>
